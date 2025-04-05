46eceea0bc57843bb74ff05a272196ec0c62fc2f
kache <kachevnikov.dev@gmail.com>
2025-04-06 00:45:36 +0300

    ReadConfig impl

--- src/main/kotlin/dev/kachvev/funpay/Main.kt
+++ src/main/kotlin/dev/kachvev/funpay/Main.kt
@@ -1,15 +1,30 @@
 package dev.kachvev.funpay
 
+import com.charleskorn.kaml.Yaml
 import dev.kachvev.extention.file.local
 import dev.kachvev.extention.profile.Profiler
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
 
 fun main() {
     val profiler = Profiler("File")
     profiler.start()
 
+    val configFile = local("config.yml")
+    println("config.yml = $configFile")
 
-    val config = local("config.yml")
-    println("config: ${config.readText()}")
+    val profiler2 = Profiler("ConfigRead")
+    profiler2.start()
+
+    val config = Yaml.default.decodeFromString<Config>(configFile.readText())
+
+    println("""
+        config.yml = ${config.readText}
+    """.trimIndent())
 
     println(profiler.stop())
+    println(profiler2.stop())
 }
+
+@Serializable
+data class Config(val readText: String)
